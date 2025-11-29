e61fec6c07cbfb252a65126fd38e15b3d8afa11e
Sarath Sajan <sarathsajan1998@gmail.com>
2025-11-29 17:02:02 +0530

    move static files to bucket

--- app_server.py
+++ app_server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,6 +16,23 @@ def devlog():
 def techstack():
     return render_template('techstack.html')
 
+def get_audio_list():
+    from google.cloud import storage
+    storage_client = storage.Client()
+    bucket_name = "seventh-wav-prod-media"
+    bucket = storage_client.bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix='audios/')
+
+    audio_data = []
+    for blob in blobs:
+        if blob.name == 'audios/':
+            continue
+        signed_url = blob.generate_signed_url(version="v4", expiration=300, method="GET")
+        filename= blob.name.split('/')[-1]
+        audio_data.append({'title':filename, 'signed_url':signed_url})
+    return audio_data
+
 @app.route("/techstack/bsroformer")
 def techstack_individual():
-    return render_template('techstack_bsroformer.html')
+    audio_data = get_audio_list()
+    return render_template('techstack_bsroformer.html', audio_list=audio_data)
