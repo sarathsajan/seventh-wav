c05ab458d1d8f29badcca32cadbc74b3848bf780
Sarath Sajan <sarathsajan1998@gmail.com>
2025-12-02 01:00:23 +0530

    modify folder structure for templates

--- app_server.py
+++ app_server.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @app.route("/devlog")
 def devlog():
